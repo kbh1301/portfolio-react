8e087fe68b8cca1d36f7725b6c959d6d0f77bcfc
kbh1301 <kyle.hulvey@gmail.com>
2022-03-30 19:05:41 -0500

    - banner css changes - resume updates

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,14 +7,14 @@ const Banner = () => {
 
     return (
         <header style={{height: '100%'}}>
-            <div id="bannerMain" className="cover bg-left bg-center-l" style={{backgroundImage: `url(${banner})`}}>
-                <div id="bannerMinor" className="bg-black-70 flex justify-center items-center w-100">
-                    <div className="w-100 fadeInContent">
-                        <div className='cusWrapper cusCusImg dib mt4 br-100 ba bw2 b--light-blue grow'>
-                            <img src={avi} className="cusWrapperImg cusImg block" alt="Avatar"/>
+            <div id="bannerMain" style={{backgroundImage: `url(${banner})`}}>
+                <div id="bannerMinor">
+                    <div className="fadeInContent" style={{width: "100%"}}>
+                        <div className='cusWrapper cusCusImg'>
+                            <img src={avi} className="cusWrapperImg cusImg" alt="Avatar"/>
                         </div>
-                        <h1 className="f2 f1-l fw2 white-90 mb0 lh-title sans-serif">My name is Kyle Hulvey</h1>
-                        <h2 className="fw1 f3 white-80 mt3 mb5 sans-serif">And I'm a Software Developer</h2>
+                        <h1 className="bannerTitle">My name is Kyle Hulvey</h1>
+                        <h2 className="bannerSubTitle">And I'm a Software Developer</h2>
                     </div>
                 </div>
             </div>
