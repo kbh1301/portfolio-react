d9755112cf68766feaaa392b6584be6879b6a81a
kbh1301 <kyle.hulvey@gmail.com>
2022-05-31 19:17:37 -0500

    Workflow test

--- src/components/Content/DemosContent/DemosContent.js
+++ src/components/Content/DemosContent/DemosContent.js
@@ -22,7 +22,7 @@ const DemosContent = () => {
                         <br/><br/>
                     </span>
                     
-                    <iframe src="https://kbh1301.github.io/barbell-calc/" width="100%" height="500px"></iframe>
+                    <iframe src="https://kbh1301.github.io/barbell-calc/" title="Barbell Calc" width="100%" height="500px"></iframe>
                 </div>
             </article>
         </div>
